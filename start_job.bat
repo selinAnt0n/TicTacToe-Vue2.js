ed68a99b3b832fdfd7f4de6de2f371470653bb70
Anton Selin <selin000ant0n@gmail.com>
2017-07-25 17:57:23 +0300

    first commit

---
+++ start_job.bat
@@ -0,0 +1,11 @@
+@echo off
+echo ___________________________
+echo ___________________________
+echo _____ START __ JOB ________
+echo ___________________________
+echo ___________________________
+start "sublime" "C:\Users\PrabuDeveloper\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Avocode\Avocode.lnk"
+start "sublime" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Sublime Text 3.lnk"
+gulp watch
+pause
+exit
